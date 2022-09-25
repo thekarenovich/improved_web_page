ae3af560371c73834d65eedcaa51332bd2ef2ff3
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-09-25 19:27:58 +0300

    Update forms.py

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -2,12 +2,13 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-
 from .models import Category, News
+from captcha.fields import CaptchaField
 import re
 
 
 class ContactForm(forms.Form):
+    recipient = forms.EmailField(label='E-mail получателя', widget=forms.EmailInput(attrs={'class': 'form-control'}))
     subject = forms.CharField(label='Тема', widget=forms.TextInput(attrs={'class': 'form-control'}))
     content = forms.CharField(label='Текст', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
 
@@ -22,25 +23,35 @@ class UserRegisterForm(UserCreationForm):
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='Подверждение пароля', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label='E-mail', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    captcha = CaptchaField(label='')
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2', 'captcha')
+
+
+class UpdateNewsFrom(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+        }
 
 
 class NewsFrom(forms.ModelForm):
     class Meta:
         model = News
-        # fields = '__all__'
-        fields = ['title', 'content', 'is_published', 'category']
+        fields = ['title', 'content', 'category']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
             'category': forms.Select(attrs={'class': 'form-control'}),
         }
 
-    def clean_title(self):
-        title = self.cleaned_data['title']
-        if re.match(r'\d', title):
-            raise ValidationError('Название не должно начинаться с цифры')
-        return title
+    # def clean_title(self):
+    #     title = self.cleaned_data['title']
+    #     if re.match(r'\d', title):
+    #         raise ValidationError('Название не должно начинаться с цифры')
+    #     return title
