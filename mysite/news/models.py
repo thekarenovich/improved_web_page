ab530da8447083c39ade35a59af0321ebfc79e9c
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-09-25 19:27:35 +0300

    Update models.py

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,13 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Наименование')
+    title = models.CharField(max_length=80, verbose_name='Наименование')
     content = models.TextField(blank=True, verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     update_at = models.DateTimeField(auto_now_add=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    views = models.IntegerField(default=0, verbose_name='Количество просмотров')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True,
                                  verbose_name='Категория')
 
@@ -30,3 +32,15 @@ class Category(models.Model):
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
         ordering = ['title']
+
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     username = models.CharField(max_length=64)
+#     password1 = models.CharField(max_length=20)
+#     password2 = models.CharField(max_length=20)
+#     email = models.EmailField()
+#
+#     class Meta:
+#         verbose_name = 'Пользователь'
+#         verbose_name_plural = 'Пользователи'
