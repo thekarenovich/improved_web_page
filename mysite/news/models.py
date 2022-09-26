d6c5bdda153b3455f6cbbb7fe46fb31780a2cd25
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-09-26 17:45:25 +0300

    Update models.py

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -12,6 +12,7 @@ class News(models.Model):
     views = models.IntegerField(default=0, verbose_name='Количество просмотров')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True,
                                  verbose_name='Категория')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author', verbose_name='Автор', default=True)
 
     def __str__(self):
         return self.title
@@ -32,15 +33,3 @@ class Category(models.Model):
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
         ordering = ['title']
-
-
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     username = models.CharField(max_length=64)
-#     password1 = models.CharField(max_length=20)
-#     password2 = models.CharField(max_length=20)
-#     email = models.EmailField()
-#
-#     class Meta:
-#         verbose_name = 'Пользователь'
-#         verbose_name_plural = 'Пользователи'
